81c1ef87e572b49acde978fcd0de21cce6d78559
Nate Miller <natekmiller96@gmail.com>
2018-10-02 22:06:55 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,12 @@
 class SessionsController < ApplicationController
   def create
+    binding.pry
     @user = User.find_or_create_by(uid: auth['uid']) do |u|
       u.name = auth['info']['name']
       u.email = auth['info']['email']
       u.image = auth['info']['image']
     end
-    binding.pry
+ 
     session[:user_id] = @user.id
  
     render 'welcome/home'
